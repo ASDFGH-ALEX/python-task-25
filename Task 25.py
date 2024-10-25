02eaa02dc4c1d4a9381438dad947de66bf1913b6
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-10-25 14:23:01 +0530

    Add files via upload

---
+++ Task 25.py
@@ -0,0 +1,14 @@
+'''
+Author:Alex Sabu
+Date:25-10-2024
+Description:Write a Python program to find the largest of three numbers. The program should take three numbers as input from the user and determine which one is the largest. Use conditional statements to compare the numbers.
+
+'''
+number1=int(input("Enter the First Number"))
+number2=int(input("Enter the Second Number"))
+if(number1>number2):
+    print(number1,"is greater than",number2)
+else:
+    print(number2,"is greaterthan",number1)
+    print("Remaining Statements")
+
